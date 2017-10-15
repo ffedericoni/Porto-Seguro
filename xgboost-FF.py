b5d410f37eb575e9d41d1c3869e09e8baf166f35
Fabrizio Federiconi <github.com@federiconi.com>
2017-10-15 17:29:46 +0200

    Add files via upload

--- xgboost-FF.py
+++ xgboost-FF.py
@@ -9,10 +9,25 @@ Created on Thu Oct  5 18:36:22 2017
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import StratifiedShuffleSplit
+from datetime import datetime
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold
+from xgboost import XGBClassifier
 #from sklearn.preprocessing import MinMaxScaler, StandardScaler
 #import lightgbm as lgb
-import xgboost as xgb
+#import xgboost as xgb
 import time
+
+def timer(start_time=None):
+    if not start_time:
+        start_time = datetime.now()
+        return start_time
+    elif start_time:
+        thour, temp_sec = divmod((datetime.now() - start_time).total_seconds(), 3600)
+        tmin, tsec = divmod(temp_sec, 60)
+        print('\n Time taken: %i hours %i minutes and %s seconds.' % (thour, tmin, round(tsec, 2)))
+
 
 # Read in our input data
 train = pd.read_csv('../input/train.csv')
@@ -46,7 +61,7 @@ li_features = ['ps_ind_13_bin', 'ps_ind_10_bin',
 #second worst 5
 li_features += ['ps_car_10_cat', 'ps_calc_20_bin',
 'ps_ind_18_bin', 'ps_calc_15_bin', 'ps_calc_16_bin']
-li_features = [] #Dont drop
+#li_features = [] #Dont drop
 train = train.drop(li_features, axis=1)
 test = test.drop(li_features, axis=1)
 
@@ -55,6 +70,7 @@ print('Test shape:', test.shape)
 
 #Multiply the most important features (indexes are based on not dropped features)
 mi_features = [35, 14, 2, 20, 34]
+mi_features = []  #No feature engineering
 mi_feature_names = ['ps_car_14', 'ps_ind_15', 'ps_ind_03',
 'ps_reg_03', 'ps_car_13']
 name = []
@@ -73,61 +89,47 @@ print('Test shape:', test.shape)
 
 X = train.values
 
-# Set xgb parameters
 
-params = {}
-params['objective'] = 'binary:logistic'
-params['eta'] = 0.03
-params['silent'] = True
-params['max_depth'] = 5  #ff era 5
-params['subsample'] = 0.9
-params['colsample_bytree'] = 0.85
-params['colsample_bylevel'] = 0.9
-params['eval_metric'] = 'auc'
-#params['tree_method'] = 'exact'
+# A parameter grid for XGBoost
+params = {
+        'min_child_weight': [3, 4, 5],
+        'gamma': [1, 1.25, 1.5],
+        'subsample': [0.6, 0.8, 1.0],
+        'colsample_bytree': [0.6, 0.8, 1.0],
+        'max_depth': [5]
+        }
 
-# Create a submission file
-sub = pd.DataFrame()
-sub['id'] = id_test
-sub['target'] = np.zeros_like(id_test)
+xgb = XGBClassifier(learning_rate=0.02, n_estimators=800, objective='binary:logistic',
+                    silent=True, missing=-1)
+folds = 5
+param_comb = 12
 
-# Take a random 30% of the dataset as validation data
+skf = StratifiedKFold(n_splits=folds, shuffle = True, random_state = 5)
+sss = StratifiedShuffleSplit(n_splits=folds, test_size=0.30, random_state=5)
 
-kfold = 5
-sss = StratifiedShuffleSplit(n_splits=kfold, test_size=0.15, random_state=15)
-for i, (train_index, test_index) in enumerate(sss.split(X, y)):
-    print('[Fold %d/%d]' % (i + 1, kfold))
-    X_train, X_valid = X[train_index], X[test_index]
-    y_train, y_valid = y[train_index], y[test_index]
-    # Convert our data into LGBoost format
-    d_train = xgb.DMatrix(X_train, y_train)
-    d_valid = xgb.DMatrix(X_valid, y_valid)
-    d_test = xgb.DMatrix(test.values)
-    watchlist = [(d_train, 'train'), (d_valid, 'valid')]
+random_search = RandomizedSearchCV(xgb, param_distributions=params, 
+                                   n_iter=param_comb, scoring='roc_auc', 
+                                   n_jobs=4, cv=sss.split(X,y), 
+                                   verbose=4, random_state=101, 
+                                   iid=True )
 
-    # Train the model! We pass in a max of 2,000 rounds (with early stopping after 100)
-    # and the custom metric (maximize=True tells xgb that higher metric is better)
-    mdl = xgb.train(params, d_train, 1600, watchlist, early_stopping_rounds=70, #feval=gini_xgb,
-                    maximize=True, verbose_eval=100)
+# Here we go
+start_time = timer(None) # timing starts from this point for "start_time" variable
+random_search.fit(X, y, early_stopping_rounds=70)
+timer(start_time) # timing ends here for "start_time" variable
 
-    print('[Fold %d/%d Prediction:]' % (i + 1, kfold))
-    # Predict on our test data
-    p_test = mdl.predict(d_test)
-    sub['target'] += p_test/kfold
+print('\n All results:')
+print(random_search.cv_results_)
+print('\n Best estimator:')
+print(random_search.best_estimator_)
+print('\n Best normalized gini score for %d-fold search with %d parameter combinations:' % (folds, param_comb))
+print(random_search.best_score_ * 2 - 1)
+print('\n Best hyperparameters:')
+print(random_search.best_params_)
+results = pd.DataFrame(random_search.cv_results_)
+y_test = random_search.predict_proba(test.values)
+results_df = pd.DataFrame(data={'id':id_test, 'target':y_test[:,1]})
+filename = 'RGS-xgb-d' + str(start_time.day) + '-h' + str(start_time.hour) + '.csv'
+results_df.to_csv(filename, index=False)
 
 
-# Create a submission file
-sub.to_csv('StratifiedShuffleSplit.csv', index=False)
-print('Elapsed Time =', time.time() - start_time)
-print('Best Score=', mdl.attr('best_score') )
-
-#Features importance
-import operator
-x = mdl.get_fscore()
-sorted_score = sorted(x.items(), key=operator.itemgetter(1))
-for ind in range(len(sorted_score)):
-    ii = int(sorted_score[ind][0].replace('f', ''))
-    print(sorted_score[ind]+(train.columns[ii],))
-#print('Features Score=', sorted_score)
-#for i,f in enumerate(train.columns):
-#    print(i,f)
