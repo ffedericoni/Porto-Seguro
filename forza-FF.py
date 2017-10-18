7e921c752407c6e49d0a55455dbf268e5922b18d
Fabrizio Federiconi <github.com@federiconi.com>
2017-10-18 17:45:27 +0200

    Add files via upload

--- forza-FF.py
+++ forza-FF.py
@@ -7,6 +7,9 @@ import xgboost as xgb
 from multiprocessing import Pool, cpu_count
 from datetime import datetime
 
+cpus=min(1, cpu_count())
+
+
 def timer(start_time=None):
     if not start_time:
         start_time = datetime.now()
@@ -19,9 +22,7 @@ def timer(start_time=None):
 train = pd.read_csv('../input/train.csv')
 test = pd.read_csv('../input/test.csv')
 col = [c for c in train.columns if c not in ['id','target']]
-print(len(col))
 col = [c for c in col if not c.startswith('ps_calc_')]
-print(len(col))
 
 train = train.replace(-1, np.NaN)
 d_median = train.median(axis=0)
@@ -35,6 +36,7 @@ def transform_df(df):
     df['ps_car_13_x_ps_reg_03'] = df['ps_car_13'] * df['ps_reg_03']
     df['negative_one_vals'] = np.sum((df[dcol]==-1).values, axis=1)
     for c in dcol:
+#        print('Now transforming col ', c)
         if '_bin' not in c: #standard arithmetic
             df[c+str('_median_range')] = (df[c].values > d_median[c]).astype(np.int)
             df[c+str('_mean_range')] = (df[c].values > d_mean[c]).astype(np.int)
@@ -50,8 +52,8 @@ def transform_df(df):
 
 def multi_transform(df):
     print('Init Shape: ', df.shape)
-    p = Pool(cpu_count())
-    df = p.map(transform_df, np.array_split(df, cpu_count()))
+    p = Pool(cpus)
+    df = p.map(transform_df, np.array_split(df, cpus))
     df = pd.concat(df, axis=0, ignore_index=True).reset_index(drop=True)
     p.close(); p.join()
     print('After Shape: ', df.shape)
@@ -68,21 +70,30 @@ def gini_xgb(pred, y):
 
 start_time = timer(None) # timing starts from this point for "start_time" variable
 
-params = {'eta': 0.02, 'max_depth': 4, 'subsample': 0.8, 'colsample_bytree': 0.8,
+seed=99
+params = {'eta': 0.02, 'max_depth': 6, 'subsample': 0.8, 'colsample_bytree': 0.8,
           'objective': 'binary:logistic', 'eval_metric': 'auc', 
-          'scale_pos_weight': 2, 'random_state': 99, 'silent': True}
-x1, x2, y1, y2 = model_selection.train_test_split(train, train['target'], test_size=0.24, random_state=99)
+          'scale_pos_weight': 31.25, 'random_state': seed, 'silent': True}
+test_perc = 0.25
+x1, x2, y1, y2 = model_selection.train_test_split(train, train['target'], 
+                                                  test_size=test_perc, 
+                                                  random_state=seed)
 
-x1 = multi_transform(x1)
-x2 = multi_transform(x2)
-test = multi_transform(test)
+#x1 = multi_transform(x1)
+#x2 = multi_transform(x2)
+#test = multi_transform(test)
+x1 = transform_df(x1)
+x2 = transform_df(x2)
+test = transform_df(test)
 
 col = [c for c in x1.columns if c not in ['id','target']]
 col = [c for c in col if not c.startswith('ps_calc_')]
 print(x1.values.shape, x2.values.shape)
 
 #remove duplicates just in case
-tdups = multi_transform(train)
+#tdups = multi_transform(train)
+tdups = transform_df(train)
+print('Train Shape: ', tdups.shape)
 dups = tdups[tdups.duplicated(subset=col, keep=False)]
 
 x1 = x1[~(x1['id'].isin(dups['id'].values))]
@@ -95,18 +106,19 @@ x1 = x1[col]
 x2 = x2[col]
 
 watchlist = [(xgb.DMatrix(x1, y1), 'train'), (xgb.DMatrix(x2, y2), 'valid')]
-model = xgb.train(params, xgb.DMatrix(x1, y1), 5000,  watchlist, #feval=gini_xgb, 
-                  maximize=True, verbose_eval=100, early_stopping_rounds=200)
+model = xgb.train(params, xgb.DMatrix(x1, y1), 327,  watchlist, #feval=gini_xgb, 
+                  maximize=True, verbose_eval=100, early_stopping_rounds=300)
 test['target'] = model.predict(xgb.DMatrix(test[col]), ntree_limit=model.best_ntree_limit+45)
 test['target'] = (np.exp(test['target'].values) - 1.0).clip(0,1)
 timer(start_time) # timing ends here for "start_time" variable
-
 filename = 'Forza-xgb-d' + str(start_time.day) + '-h' + str(start_time.hour) + '.csv'
 test[['id','target']].to_csv(filename, index=False, float_format='%.5f')
 print("Filename=", filename)
 print("Params=", params)
+print("Test % =", test_perc, "Seed =", seed)
 dumpfile = 'Forza-xgb-d' + str(start_time.day) + '-h' + str(start_time.hour) + '.dump'
-model.dump_model(dumpfile, fmap='', with_stats=True)
+model.dump_model(dumpfile, fmap='', with_stats=False)
+
 
 
 ##LightGBM
